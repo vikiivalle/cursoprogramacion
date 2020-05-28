2da0b310f5ebb5673531242fc9dc8a9f8fefca1c
vikiivalle <64442884+vikiivalle@users.noreply.github.com>
2020-05-28 08:45:35 -0300

    Update Examen test.js

--- Examen test.js
+++ Examen test.js
@@ -7,7 +7,6 @@ const libros = [
 	isbn: '9781846571510',
 	cantidadDeVecesPedido: 3,
 	estado: 'disponible',
-
 },
 { 
 	titulo: 'El abuelo que saltó por la ventana',
@@ -17,7 +16,6 @@ const libros = [
 	isbn: '9781982606336',
 	cantidadDeVecesPedido: 0,
 	estado: 'disponible',
-
 },
 {
 	titulo: 'Diario de Ana Frank',
@@ -36,8 +34,71 @@ const libros = [
 	isbn: '9781442364219',
 	cantidadDeVecesPedido: 6,
 	estado: 'no disponible',
-
 },
+{
+	titulo: 'Final del juego',
+	autor: 'Julio Cortazar',
+	anio: '1956',
+	editorial: 'Sudamericana', 
+	isbn: '9788479790691',
+	cantidadDeVecesPedido: 1,
+	estado: 'no disponible',
+},
+{
+	titulo: 'Casa tomada',
+	autor: 'Julio Cortazar',
+	anio: '1946',
+	editorial: 'Sudamericana', 
+	isbn: '978987402176',
+	cantidadDeVecesPedido: 10,
+	estado: 'disponible',
+},
+{
+	titulo: 'Bestiario',
+	autor: 'Julio Cortazar',
+	anio: '1951',
+	editorial: 'Sudamericana', 
+	isbn: '9788420417301',
+	cantidadDeVecesPedido: 1,
+	estado: 'disponible',
+},
+{
+	titulo: 'Las armas secretas',
+	autor: 'Julio Cortazar',
+	anio: '1959',
+	editorial: 'Sudamericana', 
+	isbn: '9788437601199',
+	cantidadDeVecesPedido: 8,
+	estado: 'no disponible',
+},
+{
+	titulo: 'Rayuela',
+	autor: 'Julio Cortazar',
+	anio: '1980',
+	editorial: 'Sudamericana', 
+	isbn: '9788466000512',
+	cantidadDeVecesPedido: 7,
+	estado: 'no disponible',
+},
+{
+	titulo: 'La noche boca arriba',
+	autor: 'Julio Cortazar',
+	anio: '1956',
+	editorial: 'Sudamericana', 
+	isbn: '9782253063087',
+	cantidadDeVecesPedido: 7,
+	estado: 'disponible',
+},
+{
+	titulo: 'Ficciones',
+	autor: 'Borges',
+	anio: '1944',
+	editorial: 'Sudamericana', 
+	isbn: '9789500734493',
+	cantidadDeVecesPedido: 6,
+	estado: 'disponible',
+},
+
 ];
 
 const buscarDisponibilidad = saberDisponibilidad(libros, 9781982606336)
@@ -49,4 +110,5 @@ console.log (buscarLista)
 
 const mejoresLibros = top10libros (libros)
 console.log (mejoresLibros)
+
 
